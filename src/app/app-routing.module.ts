0c5d5d10d4f0399b5f386b3564ec691a67ea20f2
Subhodeep-hash <95951344+Subhodeep-hash@users.noreply.github.com>
2021-12-24 12:20:38 +0530

    Add files via upload

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { CartComponent } from './component/cart/cart.component';
+import { ProductsComponent } from './component/products/products.component';
+import { WishListComponent } from './component/wishlist/wishlist.component';
+
+const routes: Routes = [
+  {path:'', redirectTo:'products',pathMatch:'full'},
+  {path:'products', component: ProductsComponent},
+  {path:'cart', component: CartComponent},
+  {path:'wishlist', component: WishListComponent}
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
